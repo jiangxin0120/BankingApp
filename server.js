39dbe038e8582ea7738ed3139c00e96de2b6f3e9
Xin Jiang <jiangxinxin0120@gmail.com>
2023-12-15 19:45:16 -0500

    changes

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
 
 
 // Setting the port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Starting the server
 app.listen(PORT, () => {
